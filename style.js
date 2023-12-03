d851bb0574a818b5ec6a2fb1d09668a0f6b8db54
Diya <142719131+Sudi22@users.noreply.github.com>
2023-12-03 14:49:26 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+let sidelineid = document.getElementById("memupicdn");
+let frmwrkid = document.getElementById("framepic");
+
+
+sidelineid.addEventListener("click", () => {
+    frmwrkid.classList.toggle('hidden');
+})
+
